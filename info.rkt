c6a29da80ba1466a12380c6317950b92f9a8a696
Yanying Wang <yanyingwang1@gmail.com>
2019-12-17 17:30:16 +0800

    add more code

--- info.rkt
+++ info.rkt
@@ -1,8 +1,9 @@
 #lang info
 (define collection "scribble-bl-helper")
-(define deps '("base" "gregor" "timable"))
+(define deps '("base" "gregor" "timable" "frog" "at-exp-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/scribble-bl-helper.scrbl" ())))
 (define pkg-desc "scribble helper functions especially for writing blogs")
 (define version "0.1")
 (define pkg-authors '("Yanying Wang"))
+(define raco-commands '(("frog/hp" (submod scribble-bl-helper main) "generate a frog Scribble Blog post with extended Helper functions" #f)))
--- main.rkt
+++ main.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang at-exp racket/base
 
 (module+ test
   (require rackunit))
@@ -35,23 +35,23 @@
          timable/gregor
          scribble/core)
 
-(provide frog/post/title
-         frog/post/date
-         frog/post/tags
-         frog/post/meta
-         essay-block)
+(provide title
+         date
+         tags
+         meta
+         essay)
 
 
-(define (frog/post/title . text)
+(define (title . text)
   (list* "Title: " text))
 
-(define (frog/post/date . text)
+(define (date . text)
   (list "Date: " (datetime->iso8601 (parse/datetime (car text)))))
 
-(define (frog/post/tags . text)
+(define (tags . text)
   (list "Tags: " (string-join (string-split (car text)) ", ")))
 
-(define (frog/post/meta . text)
+(define (meta . text)
   (let* ([lst (string-split (string-join text) "\n" #:trim? #t)]
          [title (first lst)]
          [date (datetime->iso8601 (parse/datetime (second lst)))]
@@ -61,7 +61,7 @@
           "Tags: " tags)))
 
 
-(define (essay-block . text)
+(define (essay . text)
   (let* ([seperator1 (index-of text "\n")]
           [lst1 (take text seperator1)]
           [lst1r (drop text (add1 seperator1))]
@@ -92,11 +92,45 @@
   ;; does not run when this file is required by another module. Documentation:
   ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
 
-  (require racket/cmdline)
-  (define who (box "world"))
+  (require racket/cmdline
+           raco/command-name
+           racket/system
+           racket/file
+           gregor)
+
+  (require debug/repl)
   (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
+   #:program (short-program+command-name) #;"raco frog/sbh"
+   #:once-each
+   [("-n" "--name")
+    title
+    "your title name of the new post"
+    (or (file-exists? (build-path (current-directory) "frog.rkt"))
+        (error "`frog.rkt` not found, you may want to run `raco frog --init` first."))
+    (define filename @string-append{_src/posts/@|(date->iso8601 (current-date))|-@|title|.scrbl})
+    (define filepath (build-path (current-directory) filename))
+    (and (file-exists? filepath)
+         (error (~a "file already exist: " filepath)))
+    (define filecontent @~a{#lang scribble/manual
+
+                            @"@"(require scribble-bl-helper)
+                            @"@"(require (for-label racket)) @"@"; remove this line if no racket doc links needed
+
+                            @"@"title{@title}
+                            @"@"date{@(datetime->iso8601 (now))}
+                            @"@"tags{DRAFT tag1 tag2}
+
+
+                            Replace this with your post text. Add one or more comma-separated
+                            Tags above. The special tag `DRAFT` will prevent the post from being
+                            published. And text before the `more` line will appear in the post
+                            index page as well.
+
+                            <!-- more -->
+
+                            @"@"(table-of-contents) @"@"; remove this line if no toc neeeded.
+
+                            You blog content continues here.
+                            })
+    (display-to-file filecontent filepath)
+    (displayln filepath)]))
